cb6d720e847206503f414e870bd6e8e00c1656e2
koheinthurako <captainthura99@gmail.com>
2023-11-14 23:58:48 +0630

    Final state concept is fine!

--- src/TestAssignments/PrimeNumbers.java
+++ src/TestAssignments/PrimeNumbers.java
@@ -10,6 +10,9 @@ public class PrimeNumbers {
 		boolean checker = true;
 		
 		while(checker) {
+			
+			System.out.println("Please Enter a number...");
+			
 			if(sc.hasNextInt()) {
 				int userInput = sc.nextInt();
 				boolean result = false;
@@ -18,26 +21,45 @@ public class PrimeNumbers {
 					
 					if(userInput==i) {
 						continue;
+					} else if(userInput==1) {
+						System.out.println("You can't type " + userInput + " please type other numbers");
+						break;
 					} else if(userInput%i==0) {
 						System.out.println(userInput + " is not prime number!");
-						checker = false;
 						result = false;
 						break;
 					} else {
 						result = true;
 					}
-					
 				}
 				
-				if(result) {
-					System.out.println("Yes! " + userInput + " is prime number!");
-					break;
+				if(result) System.out.println("Yes! " + userInput + " is prime number!");
+				
+				System.out.println("Want to check another Y/N : ");
+				sc.nextLine();
+				String userInput2 = sc.nextLine().toLowerCase();
+				if(userInput2.equals("y")) {
+					checker = true;
+				} else {
+					System.out.println("--- Program Ended! ---");
+					checker = false;
 				}
 				
 			} else {
-				System.out.println("Invalid Value!");
-				break;
+				System.out.println("Invalid Value! Please enter only numbers");
+				System.out.println("Please type numbers : ");
+				sc.nextLine();
+				String userInput2 = sc.nextLine().toLowerCase();
+				if(userInput2.equals("y")) {
+					checker = true;
+				} else {
+					System.out.println("--- Program Ended! ---");
+					checker = false;
+				}
+				
+//				break;
 			}
+			
 		}
 		
 		sc.close();
