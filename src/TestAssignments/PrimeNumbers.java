c20c756bf453aea77971c0ac6c85bb14f7e0961e
koheinthurako <captainthura99@gmail.com>
2023-11-14 22:37:10 +0630

    First state for prime

--- src/TestAssignments/PrimeNumbers.java
+++ src/TestAssignments/PrimeNumbers.java
@@ -10,7 +10,19 @@ public class PrimeNumbers {
 		
 		while(true) {
 			if(sc.hasNextInt()) {
-				System.out.println("True");
+				int userInput = sc.nextInt();
+				
+				for(int i = 3; i < 11; i++) {
+					if(userInput%i==0) {
+						System.out.println(i + " is not prime number!");
+						System.out.println("Even");
+					} else {
+//						System.out.println("Succeeded!");
+//						System.out.println("Odd");
+//						System.out.println("--- Program Ended ---");
+						
+					}
+				}
 			} else {
 				System.out.println("Invalid Value!");
 				break;
