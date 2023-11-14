0717ece332f3ed5afe49352130f2e2c89c0ff07d
koheinthurako <captainthura99@gmail.com>
2023-11-14 23:10:37 +0630

    Second state

--- src/TestAssignments/PrimeNumbers.java
+++ src/TestAssignments/PrimeNumbers.java
@@ -7,22 +7,28 @@ public class PrimeNumbers {
 		System.out.println("Let's check prime numbers! Program start...");
 		
 		Scanner sc = new Scanner(System.in);
+		boolean checker = true;
 		
-		while(true) {
+		while(checker) {
 			if(sc.hasNextInt()) {
 				int userInput = sc.nextInt();
+				boolean result = false;
 				
 				for(int i = 3; i < 11; i++) {
-					if(userInput%i==0) {
-						System.out.println(i + " is not prime number!");
-						System.out.println("Even");
+					
+					if(userInput==i) {
+						continue;
+					} else if(userInput%i==0) {
+						System.out.println(userInput + " is not prime number!");
+						checker = false;
+						return;
 					} else {
-//						System.out.println("Succeeded!");
-//						System.out.println("Odd");
-//						System.out.println("--- Program Ended ---");
-						
+						result = true;
 					}
 				}
+				
+				if(result) System.out.println("Yes! " + userInput + " is prime number!");
+				
 			} else {
 				System.out.println("Invalid Value!");
 				break;
