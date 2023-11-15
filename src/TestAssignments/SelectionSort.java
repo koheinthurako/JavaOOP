8323dc43a3b1e23f11105a632e2b17c509d80edb
koheinthurako <captainthura99@gmail.com>
2023-11-15 23:43:11 +0630

    add selectionSort

---
+++ src/TestAssignments/SelectionSort.java
@@ -0,0 +1,15 @@
+package TestAssignments;
+
+// "Declare the array recorded with 10 values (3,5,2,7,9,10,15,7,0,4). Sort the array by using Selection algorithms."
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		
+		int [] nums = {3, 5, 2, 7, 9, 10, 15, 7, 0, 4};
+		
+		
+		
+	}
+
+}
