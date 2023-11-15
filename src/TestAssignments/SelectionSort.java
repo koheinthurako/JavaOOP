6522b95389e4168442beb3f3683a005336ae665f
koheinthurako <captainthura99@gmail.com>
2023-11-15 23:56:58 +0630

    Second State

--- src/TestAssignments/SelectionSort.java
+++ src/TestAssignments/SelectionSort.java
@@ -10,14 +10,19 @@ public class SelectionSort {
 		
 		int [] nums = {3, 5, 2, 7, 9, 10, 15, 7, 0, 4};
 		
-		for(int i = 0; i < nums.length - 1; i++) {
-			if(nums[i] > nums[i+1]) {
-				int temp = nums[i];
-				nums[i] = nums[i+1];
-				nums[i+1] = temp;
-			}
+//		for(int i = 0; i < nums.length - 1; i++) {
+//			if(nums[i] > nums[i+1]) {
+//				int temp = nums[i];
+//				nums[i] = nums[i+1];
+//				nums[i+1] = temp;
+//			}
+//		}
+		
+		for(int i = 0; i <= nums.length - 1; i++) {
+			System.out.print(i + " ");
 		}		
-		System.out.println(Arrays.toString(nums));
+		
+//		System.out.println(Arrays.toString(nums));
 		
 	}
 
