731d0f8df2d47bbfbf604dd8b3554bcb144c40fe
koheinthurako <captainthura99@gmail.com>
2023-11-16 22:08:03 +0630

    delete linearsearch and update lin

--- src/TestAssignments/LinearSearch.java
+++ src/TestAssignments/LinearSearch.java
@@ -1,87 +1,74 @@
 package TestAssignments;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class LinearSearch {
-
+	
 	public static void main(String[] args) {
+		
+		System.out.println("Let's find the numbers from an array!");
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int [] nums = {3, 5, 2, 7, 9, 10, 15, 7, 0, 4};
 		
-		System.out.println("Let's find the numbers from an array!");
+		boolean mainController = true;
 		
-		ArrayList<Integer> dataList = new ArrayList<>();
-//		
-		boolean checker = true;
-//		
-//		while(checker) {
-//			System.out.println("Please type a number...");
-//			if(sc.hasNextInt()) {
-//				int input = sc.nextInt();	
-//				for(int i = 0; i < nums.length; i++) {
-//					if(input == nums[i]) {
-//						System.out.println(input + " is founded at array index : " + i);
-//						dataList.add(nums[i]);
-//					}
-//				}
-//				if(dataList.isEmpty()) {
-//					System.out.println(-1);
-//					System.out.println("Do you want to search the program? Y/N : ");
-//					sc.nextLine();
-//					String yesOrNo = sc.nextLine().toLowerCase();
-//					if(yesOrNo.equals("y")) checker = true;
-//					else {
-//						checker = false;
-//						System.out.println("Have a nice day!");
-//						System.out.println("--- Program End ---");
-//					};
-//				};
-//			} else {
-//				System.out.println("Invalid Value! Please type numbers only...");
-//				System.out.println("Do you want to restart the program? Y/N : ");
-//				sc.nextLine();
-//				String yesOrNo = sc.nextLine().toLowerCase();
-//				if(yesOrNo.equals("y")) checker = true;
-//				else {
-//					checker = false;
-//					System.out.println("Have a nice day!");
-//					System.out.println("--- Program End ---");
-//				};
-//			}
-//		}
-		
-		
-		
-		
-		
-		if(sc.hasNextInt()) {
-			while(checker) {
+		//Start outer loop
+		while(mainController) {
+			System.out.println("Please type a number...");
+			
+			// Check integer or not
+			if(sc.hasNextInt()) {
 				int input = sc.nextInt();	
+				boolean checker = false;
+				
+				// Start inner loop
 				for(int i = 0; i < nums.length; i++) {
 					if(input == nums[i]) {
 						System.out.println(input + " is founded at array index : " + i);
-						dataList.add(nums[i]);
+						checker = true;
 					}
 				}
-				if(dataList.isEmpty()) System.out.println(-1);
+				
+				// Ask user to continue or not
+				System.out.println("Do you want to search the program? Y/N : ");
+				sc.nextLine();
+				String yesOrNo = sc.nextLine().toLowerCase();
+				if(yesOrNo.equals("y")) mainController = true;
+				else {
+					mainController = false;
+					System.out.println("Have a nice day!");
+					System.out.println("--- Program End ---");
+				};
+				
+				// Print -1 and ask user to continue or not
+				if(!checker) {
+					System.out.println("-1");
+					System.out.println("Do you want to search the program? Y/N : ");
+					sc.nextLine();
+					yesOrNo = sc.nextLine().toLowerCase();
+					if(yesOrNo.equals("y")) mainController = true;
+					else {
+						mainController = false;
+						System.out.println("Have a nice day!");
+						System.out.println("--- Program End ---");
+					};
+				}
+				
+			// If userInput is not integer, print invalid value and ask user to continue or not
+			} else {
+				System.out.println("Invalid Value! Please type numbers only...");
+				System.out.println("Do you want to restart the program? Y/N : ");
+				sc.nextLine();
+				String yesOrNo = sc.nextLine().toLowerCase();
+				if(yesOrNo.equals("y")) mainController = true;
+				else {
+					mainController = false;
+					System.out.println("Have a nice day!");
+					System.out.println("--- Program End ---");
+				};
 			}
-		} else {
-			System.out.println("Invalid Value! Please type numbers only...");
-			System.out.println("Do you want to restart the program? Y/N : ");
-			sc.nextLine();
-			String yesOrNo = sc.nextLine().toLowerCase();
-			if(yesOrNo.equals("y")) checker = true;
-			else {
-				checker = false;
-				System.out.println("Have a nice day!");
-				System.out.println("--- Program End ---");
-			};
 		}
-		
-		
 		sc.close();
 	}
 }
