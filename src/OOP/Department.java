c8e2286951bb407fafec52cd789100db4aea64a2
koheinthurako <captainthura99@gmail.com>
2023-11-18 22:59:00 +0630

    start oop concepts

--- src/OOP/Department.java
+++ src/OOP/Department.java
@@ -2,8 +2,38 @@ package OOP;
 
 public class Department {
 	
-	private int a = 10;
-	private String name = "Ko";
-	private boolean gender = true;
+	private int age;
+	private String name;
+	private boolean gender;
+	
+	public Department(int a, String name, boolean gender) {
+		this.age = a;
+		this.name = name;
+		this.gender = gender;
+	}
 
+	public int getA() {
+		return age;
+	}
+
+	public void setA(int a) {
+		this.age = a;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	
 }
--- src/OOP/DepartmentTest.java
+++ src/OOP/DepartmentTest.java
@@ -4,9 +4,9 @@ public class DepartmentTest {
 
 	public static void main(String[] args) {
 		
-		Department dp1;
+		Department dp1 = new Department(25, "Thet Zaw Hein", true);
 		
-		System.out.println();
+		System.out.println(dp1.getA());
 		
 	}
 	
