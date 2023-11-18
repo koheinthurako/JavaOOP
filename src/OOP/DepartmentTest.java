6e9c3e637a48bf38c232f162f16716dea0f88b3f
koheinthurako <captainthura99@gmail.com>
2023-11-19 00:00:03 +0630

    add note again

--- src/OOP/DepartmentTest.java
+++ src/OOP/DepartmentTest.java
@@ -17,11 +17,10 @@ public class DepartmentTest {
 		အဲ့ကနေ သူ့ class ထဲမှာရှိတဲ့ plusAge ကို return ပြန်ပေးလို့ 5 ရလာတယ် အဲ့ 5 က checkDp.dp.changeAge(5)
 		ဆိုပြီးဖြစ်သွားတယ် changeAge ကိုခေါ်ဖို့အတွက် checkDp ထဲက dp obj ကိုသွားခေါ်တော့ dp က Department class
 		ရဲ့ constructor နဲ့ obj အသစ်ဆောက်ထားလို့ dp ရှိတဲ့ Department.java ထဲကိုပြန်ဆင်းသွားပြီး changeAge(5) method
-		ကိုသွားခေါ်တယ် parameter ထဲက 5 နဲ့ မူရင်း Department 
-		အဲ့ဒီ့ object checkDp ထဲက dp ဆိုတဲ့ constructor ကိုသွားခေါ်ပြီး 
-		Department ထဲကိုဆက်ဆင်းသွားပြီး department ထဲက method ကိုအလုပ်လုပ်
+		ကိုသွားခေါ်တယ် parameter ထဲက 5 နဲ့ မူရင်း Department ထဲမှာရှိတဲ့ age တန်ဖိုးနဲ့သွားပေါင်းပြီး return အသစ်ပြန်ပေးတယ်
+		ပြီးမှ အောက်က checkDp ကို print ထုတ်လိုက်တော့ checkDp ထဲက toString() method ထဲကမှတစ်ဆင့် dp ခံထားလို့ dp
+		ထဲက toString ထဲကိုထပ်ဆင်းသွားပြီး string တွေအားလုံးပေါင်းထုတ်ပေးတယ်
 */
-		
 		System.out.println(checkDp);
 		
 //		System.out.println(dp1.getA());
