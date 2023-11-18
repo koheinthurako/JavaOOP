1c8d76a7ca14e34d5ae8f5f9d7ff6320123abd99
koheinthurako <captainthura99@gmail.com>
2023-11-19 00:03:26 +0630

    finish about method propagation

--- src/OOP/DepartmentTest.java
+++ src/OOP/DepartmentTest.java
@@ -13,13 +13,14 @@ public class DepartmentTest {
 		checkDp.dp.changeAge(checkDp.getPlusAge());
 /*
 		Method Propagation အကြောင်းအတွက်
-		အောက်က codeline ကို ရှင်းမယ် အရင်ဆုံး checkDp obj ထဲက getPlusAge() getter method ကိုသွားခေါ်တယ်
+		အပေါ်က codeline ကို ရှင်းမယ် အရင်ဆုံး checkDp obj ထဲက getPlusAge() ဆိုတဲ့ getter method ကိုသွားခေါ်တယ်
 		အဲ့ကနေ သူ့ class ထဲမှာရှိတဲ့ plusAge ကို return ပြန်ပေးလို့ 5 ရလာတယ် အဲ့ 5 က checkDp.dp.changeAge(5)
-		ဆိုပြီးဖြစ်သွားတယ် changeAge ကိုခေါ်ဖို့အတွက် checkDp ထဲက dp obj ကိုသွားခေါ်တော့ dp က Department class
-		ရဲ့ constructor နဲ့ obj အသစ်ဆောက်ထားလို့ dp ရှိတဲ့ Department.java ထဲကိုပြန်ဆင်းသွားပြီး changeAge(5) method
-		ကိုသွားခေါ်တယ် parameter ထဲက 5 နဲ့ မူရင်း Department ထဲမှာရှိတဲ့ age တန်ဖိုးနဲ့သွားပေါင်းပြီး return အသစ်ပြန်ပေးတယ်
-		ပြီးမှ အောက်က checkDp ကို print ထုတ်လိုက်တော့ checkDp ထဲက toString() method ထဲကမှတစ်ဆင့် dp ခံထားလို့ dp
-		ထဲက toString ထဲကိုထပ်ဆင်းသွားပြီး string တွေအားလုံးပေါင်းထုတ်ပေးတယ်
+		ဆိုပြီးဖြစ်သွားတယ် changeAge ကိုခေါ်ဖို့အတွက် checkDp.dp ဆိုတဲ့အတိုင်း checkDp ထဲက dp obj ကိုသွားခေါ်တော့ dp က
+		Department class ရဲ့ constructor နဲ့ obj အသစ်ဆောက်ထားလို့ dp ရှိတဲ့ Department.java ထဲကိုပြန်ဆင်းသွားပြီး
+		changeAge(5) method ကိုသွားခေါ်တယ် parameter ထဲက 5 နဲ့ မူရင်း Department.java ထဲမှာရှိတဲ့ age တန်ဖိုးနဲ့သွားပေါင်းပြီး
+		return အသစ်ပြန်ပေးတယ် ပြီးမှ အောက်က checkDp ကို print ထုတ်လိုက်တော့ checkDp ထဲက toString() method
+		ထဲကမှတစ်ဆင့် dp ခံထားလို့ dp ထဲက toString ထဲကိုထပ်ဆင်းသွားပြီး string တွေအားလုံးပေါင်းထုတ်ပေးတယ်
+		method တွေကို တစ်ဆင့်ကနေတစ်ဆင့် ပွားပြီးဆင်းသွားတဲ့အတွက် method propagation လို့ခေါ်တယ်
 */
 		System.out.println(checkDp);
 		
