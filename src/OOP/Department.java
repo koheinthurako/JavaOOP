7e659bc9e56156d2fc386ce0c1a3c1f4ffd2e302
koheinthurako <captainthura99@gmail.com>
2023-11-18 22:55:10 +0630

    add departmentTest and update

--- src/OOP/Department.java
+++ src/OOP/Department.java
@@ -3,6 +3,7 @@ package OOP;
 public class Department {
 	
 	private int a = 10;
-	
+	private String name = "Ko";
+	private boolean gender = true;
 
 }
---
+++ src/OOP/DepartmentTest.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class DepartmentTest {
+
+	public static void main(String[] args) {
+		
+		Department dp1;
+		
+		System.out.println();
+		
+	}
+	
+}
