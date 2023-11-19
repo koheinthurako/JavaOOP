f41839d4c2102fcae0681a56e3461846cacd298a
koheinthurako <captainthura99@gmail.com>
2023-11-19 23:08:00 +0630

    add con, getsetter and tostring

--- src/Inheritence/Shape.java
+++ src/Inheritence/Shape.java
@@ -2,15 +2,50 @@ package Inheritence;
 
 public class Shape {
 	
+	private int width;
+	private int height;
+	private int radius;
+	
 	public static void main(String[] args) {
-		
-		private int width;
-		private int height;
-		private int radius;
-		private final int CONST;
 		
 		
 		
 	}
 
+	public Shape(int width, int height, int radius) {
+		super();
+		this.width = width;
+		this.height = height;
+		this.radius = radius;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public String toString() {
+		return "Shape [width=" + width + ", height=" + height + ", radius=" + radius + "]";
+	}
+	
 }
