6cff72c744efbae47c1b2c4b79a49dc5eec25388
koheinthurako <captainthura99@gmail.com>
2023-11-19 23:05:42 +0630

    Start inheritence

---
+++ src/Inheritence/Shape.java
@@ -0,0 +1,16 @@
+package Inheritence;
+
+public class Shape {
+	
+	public static void main(String[] args) {
+		
+		private int width;
+		private int height;
+		private int radius;
+		private final int CONST;
+		
+		
+		
+	}
+
+}
