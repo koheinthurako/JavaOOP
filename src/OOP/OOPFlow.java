ed7e2ef099c9c2eda5c739a866c3ebc7659f2a1b
koheinthurako <captainthura99@gmail.com>
2023-11-20 21:49:09 +0630

    add OOP flow notes

---
+++ src/OOP/OOPFlow.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class OOPFlow {
+	
+//	OOP Flow သွားသည့်ပုံစံ
+	
+	// 1. Attributes
+	// 2. Constructors;
+	// 3. method return type, method parameter passing, method overloading, method recursive
+	
+	
+
+}
