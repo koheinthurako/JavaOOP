6a9984e4dad479ac92ad8f686d6b7718cc6333c6
koheinthurako <captainthura99@gmail.com>
2023-11-20 22:13:22 +0630

    Start access modifier

--- src/Inheritence/Shape.java
+++ src/Inheritence/Shape.java
@@ -2,50 +2,16 @@ package Inheritence;
 
 public class Shape {
 	
-	private int width;
-	private int height;
-	private int radius;
+	
 	
 	public static void main(String[] args) {
 		
+		Square sq = new Square(1, 3, 3);
+		sq.setWidth(100);
+		System.out.println(sq);
 		
-		
 	}
 
-	public Shape(int width, int height, int radius) {
-		super();
-		this.width = width;
-		this.height = height;
-		this.radius = radius;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
-	public int getRadius() {
-		return radius;
-	}
-
-	public void setRadius(int radius) {
-		this.radius = radius;
-	}
-
-	@Override
-	public String toString() {
-		return "Shape [width=" + width + ", height=" + height + ", radius=" + radius + "]";
-	}
+	
 	
 }
--- src/Inheritence/Square.java
+++ src/Inheritence/Square.java
@@ -1,8 +1,48 @@
 package Inheritence;
 
-public class Square{
+public class Square {
+	
+	private int width;
+	private int height;
+	private int radius;
 	
 	public Square(int width, int height, int radius) {
+		this.width = width;
+		this.height = height;
+		this.radius = radius;
+	}
+
+	// Access Modifier - Getter/ Setter
+	public int getWidth() {
+		return width;
+	}
+
+	// set လုပ်လိုက်တဲ့ value ကို condition စစ်ပြီးမှ လက်ခံခြင်း
+	public void setWidth(int width) {
+		if(width > 0 & width<100)
+		this.width = width;
+		else this.width = 1;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public String toString() {
+		return "Shape [width=" + width + ", height=" + height + ", radius=" + radius + "]";
 	}
 
 }
