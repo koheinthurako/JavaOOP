ae8a639a8663b69daa2eaa493cc2b5579dc98160
koheinthurako <captainthura99@gmail.com>
2023-11-20 22:36:46 +0630

    take notes about modifier

--- src/OOP/DepartmentTest.java
+++ src/OOP/DepartmentTest.java
@@ -5,6 +5,7 @@ public class DepartmentTest {
 	public static void main(String[] args) {
 		
 		Department dp1 = new Department(20, "Thet Zaw Hein", true);
+		
 //		ဒီနေရာမှာ အရင် CheckeDept ကို obj ဆောက်လိုက်တယ် 
 		CheckeDept checkDp = new CheckeDept();
 		System.out.println("Additional age = " + checkDp.getPlusAge());
--- src/OOP/OOPFlow.java
+++ src/OOP/OOPFlow.java
@@ -2,11 +2,13 @@ package OOP;
 
 public class OOPFlow {
 	
-//	OOP Flow သွားသည့်ပုံစံ
+//	OOP Flow code များရေးသည့်ပုံစံ
 	
-	// 1. Attributes
-	// 2. Constructors;
-	// 3. method return type, method parameter passing, method overloading, method recursive
+//	1. Attributes
+//	2. Constructors;
+//	3. Access Modifier => eg.getter/setter;
+//	4. method return type, method parameter passing, method overloading, method recursive
+//	5. toString()
 	
 	
 
