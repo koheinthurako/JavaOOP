c7e9f261ef990c1546bcbe772b79221298b8f5d9
koheinthurako <captainthura99@gmail.com>
2023-11-21 23:08:11 +0630

    finishing rectangle area

--- src/RectangleAggregationArea/Length.java
+++ src/RectangleAggregationArea/Length.java
@@ -7,6 +7,10 @@ public class Length {
 	public static final int CONST1 = 12;
 	public static final int CONST2 = 144;
 	
+	public Length() {
+		
+	}
+	
 	public Length(int feet, int inch) {
 		this.feet = feet;
 		this.inch = inch;
@@ -32,12 +36,16 @@ public class Length {
 //		ဒီနေရာမှာ သတိထားပါ!!!!! ဒီ method က အစတုန်းက String method
 //		toString() method ကနေ int နှစ်လုံးကို return ပြန်ပေးနိုင်ဖို့ Length class ကို object ဆောက်လိုက်တယ် return မှာ
 //		obj ကြီးကိုသာ return ပြန်လိုက်ရင် Length class ရဲ့ toString က အလုပ်လုပ်နိုင်သွားမယ်
-		int f = Math.round(inches/CONST2);
-		int i = Math.round((inches%CONST2)/CONST1);
+		
+//		int f = Math.round(inches/CONST2);
+//		int i = Math.round((inches%CONST2)/CONST1);
+		
 //		f နဲ့ i value က int တွေဖြစ်တဲ့အတွက် return မှာအဆင်ပြေအောင် l obj အနေနဲ့ တန်ဖိုးနှစ်ခုလုံးပေးလိုက်တယ် return က l object
 //		ဖြစ်သွားတဲ့အတွက် method ကလည်း Length type ကိုပြောင်းသွားတာကို သတိထားရမယ် !!!
-		Length l = new Length(f, i);
-		return l;
+//		Length l = new Length(f, i);
+		
+//		အပေါ်က code တွေကို အတိုချုပ် return ပြန်မယ်ဆို ဒီလိုဖြစ်မယ်
+		return new Length(Math.round(inches/CONST2), Math.round((inches%CONST2)/CONST1));
 	}
 	
 	@Override
--- src/RectangleAggregationArea/Rectangle.java
+++ src/RectangleAggregationArea/Rectangle.java
@@ -37,10 +37,11 @@ public class Rectangle {
 	}
 	
 	public void calcArea() {
-//		System.out.println("Area = " + width.convertInches() * height.convertInches());
 		int area = width.convertInches() * height.convertInches();
-		System.out.println(width.convertLength(area));
-//		ဒီနေရာမှာ
+		Length len = new Length();
+		System.out.println("Area = " + len.convertLength(area));
+//		ဒီနေရာမှာ Length.java ထဲက converLength() က return ပြန်လိုက်တော့ printLn(l) ဆိုပြီးဖြစ်သွားတယ် print ထုတ်မယ်ဆိုတော့ l က
+//		Length object ကြီးဖြစ်နေလို့ Length class ထဲက toString() method ထဲမှာ l ထဲက ပါလာတဲ့ int နှစ်ခုက ဝင်သွားပြီး print ထုတ်သွားတယ်
 	}
 
 	@Override
