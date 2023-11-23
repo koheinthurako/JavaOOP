28b4b5f59214531a28134fe6bb874ca2c516721a
koheinthurako <captainthura99@gmail.com>
2023-11-23 19:55:57 +0630

    delete unnecessary files

--- src/DurationChecker/DurationCalculation.java
+++ src/DurationChecker/DurationCalculation.java
@@ -4,7 +4,7 @@ public class DurationCalculation {
 
 	public static void main(String[] args) {
 
-		Duration duration = new Duration(new TimeLine(9, 35, 00), new TimeLine(11, 30, 05));
+		Duration duration = new Duration(new TimeLine(9, 35, 00), new TimeLine(13, 30, 05));
 		System.out.println(duration);
 		duration.calcDuration();
 
