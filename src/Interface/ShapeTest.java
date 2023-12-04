3e0aa7d87139f588605e3685409f545c30fc6deb
koheinthurako <captainthura99@gmail.com>
2023-12-04 18:34:20 +0630

    start about interface

--- src/Interface/ShapeTest.java
+++ src/Interface/ShapeTest.java
@@ -6,7 +6,7 @@ public class ShapeTest {
 	public static void main(String[] args) {
 		
 		Length l1 = new Length(1, 2);
-		Length l2 = new Length(2, 3);
+//		Length l2 = new Length(2, 3);
 
 //		Interface တွင်လည်း constructor ကနေ object ဆောက်လို့မရပါ
 		Shape s = new Circle(l1);
