452fa1d96b4705591af797cc8443de43760f7ba0
koheinthurako <captainthura99@gmail.com>
2023-12-06 20:47:03 +0630

    concept finish

--- src/EmployeeInfo/CompanyDataChecker.java
+++ src/EmployeeInfo/CompanyDataChecker.java
@@ -3,11 +3,13 @@ package EmployeeInfo;
 public class CompanyDataChecker {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Employees' Data \n====================\n");
 
-		Employee emp1 = new Employee("220001", "Thet Zaw Hein");
+		Employee emp1 = new Employee("110001", "Thet Zaw Hein");
 		System.out.println(emp1.userInformation(emp1.getId()));
 		
-		Employee emp2 = new Employee("120003", "Ko Hein");
+		Employee emp2 = new Employee("410003", "Ko Hein");
 		System.out.println(emp2.userInformation(emp2.getId()));
 		
 	}
--- src/EmployeeInfo/Employee.java
+++ src/EmployeeInfo/Employee.java
@@ -1,9 +1,11 @@
 package EmployeeInfo;
 
 public class Employee {
-	
+
 	private String id;
 	private String name;
+	
+	public Employee () {};
 
 	public Employee(String id, String name) {
 		this.id = id;
@@ -27,51 +29,63 @@ public class Employee {
 	}
 
 	public String userInformation(String id) {
-		
+
 		class ID {
 			private char countryCode = id.charAt(0);
 			private char gender = id.charAt(1);
 			private String entrance_no = id.substring(2);
 
+			public boolean checkGender(String country) {
+				if (gender == CompanyData.MALE) {
+					String confirmGender = "MALE";
+					show_Employee_Information(country, confirmGender);
+				} else if (gender == CompanyData.FEMALE) {
+					String confirmGender = "FEMALE";
+					show_Employee_Information(country, confirmGender);
+				} else {
+					toString();
+					System.out.println(CompanyData.INVALID_USER_INF());
+				}
+				return true;
+			}
+
 			public boolean checkID() {
+				System.out.println("Name : " + name);
 				if (countryCode == CompanyData.JAPAN) {
-					show_Employee_Information(countryCode, gender, entrance_no);
+					String country = "JAPAN";
+					checkGender(country);
 					return true;
 				} else if (countryCode == CompanyData.MYAN) {
-					show_Employee_Information(countryCode, gender, entrance_no);
+					String country = "MYANMAR";
+					checkGender(country);
 					return true;
 				} else if (countryCode == CompanyData.OTHER) {
-					show_Employee_Information(countryCode, gender, entrance_no);
+					String country = "Other countries...";
+					checkGender(country);
 					return true;
 				} else {
-					System.out.println(countryCode + " = last condition");
+					System.out.println(CompanyData.INVALID_USER_INF());
 					return false;
 				}
 			}
 
-			public String show_Employee_Information(char country, char gender, String entrance_no) {
-				System.out.println("Country : " + countryCode);
+			public String show_Employee_Information(String country, String gender) {
+				System.out.println("Country : " + country);
 				System.out.println("Gender : " + gender);
 				System.out.println("Entrance_No : " + entrance_no);
 				return "";
 			}
 		}
+
 		ID checkInfo = new ID();
 		checkInfo.checkID();
-//		if (checkInfo.checkID()) {
-//			checkInfo.show_Employee_Information();
-//			System.out.println("true");
-//		} else {
-//			CompanyData.INVALID_USER_INF();
-//		}
-		
+
 		return toString();
-		
 	}
 
 	@Override
 	public String toString() {
 		return "";
 	}
-	
+
 }
