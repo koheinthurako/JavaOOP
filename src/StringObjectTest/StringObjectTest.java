97833885b52543ddb3642a8ffe4f898741fc74f0
koheinthurako <captainthura99@gmail.com>
2023-12-08 21:50:57 +0630

    start Testing

--- src/StringObjectTest/StringObjectTest.java
+++ src/StringObjectTest/StringObjectTest.java
@@ -6,8 +6,20 @@ public class StringObjectTest {
 
 		String st = "Ko Hein";
 		String st2 = "Thet";
+		StringObjectTest stObj = new StringObjectTest();
 		
+		System.out.println(st.getClass());
+		System.out.println(stObj.getClass().getSimpleName());
 		
+		System.out.println("String charAt : " + st.charAt(1));
+		System.out.println("String endsWith : " + st.endsWith("y"));
+		System.out.println("String indexOf(subString) : " + st.indexOf(st.substring(5)));
+		System.out.println("String indexOf (subString and fromIndex) : " + st.indexOf(st.substring(7), 1));
+		
+//		Substring beginIndex
+		System.out.println("String substring beginIndex : " + st.substring(4));
+//		Substring beginIndex and endIndex => 3 လုံးမြောက်ပြည့်တဲ့နေရာထိအရင်ဖြတ်ပြီး ၅ လုံးမြောက်နေရာရဲ့နောက်ပိုင်းကိုအကုန်ဖြတ်ချ
+		System.out.println("String substring beginIndex and endIndex : " + st.substring(3, 5));		
 		
 	}
 
