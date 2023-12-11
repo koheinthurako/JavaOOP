d9f09bc200a4e2ab6e0b29757c0769c0cfdfd9ac
koheinthurako <captainthura99@gmail.com>
2023-12-11 22:17:44 +0630

    start java.lang.Math

---
+++ src/API/MathObjectTest.java
@@ -0,0 +1,15 @@
+package API;
+
+public class MathObjectTest {
+
+	public static void main(String[] args) {
+
+//		Java.lang.Math အကြောင်းစမယ်
+		
+//		Math.ceil() ကို ဒီတိုင်း Math ဆိုပြီးယူသုံးလို့ရတယ် Math.ceil() က 1 ကျော်တာနဲ့  1 တိုးပေးတယ်
+//		ဒီနေရာမှာ သတိထားရမှာက Math တို့ Scanner class တို့က final class ဖြစ်လို့ extends ဆိုပြီး အမွေဆက်ခံလို့မရပါ
+		System.out.println(Math.ceil(1.3));
+		
+	}
+
+}
--- src/API/Palindrome.java
+++ src/API/Palindrome.java
@@ -15,16 +15,23 @@ public class Palindrome {
 		String st = sc.nextLine();
 //		StringBuildeသည် mutable ဖြစ်တယ်
 		StringBuilder sb = new StringBuilder(st);
-//		mutable ဖြစ်လို့ အောက်မှာ reverse ပြန်လိုက်တော့ inputBuilder ထဲကို reverse ပြန်ထားတဲ့တန်ဖိုးကဝင်သွားတယ်
+//		mutable ဖြစ်လို့ အောက်မှာ reverse ပြန်လိုက်တော့ sb ထဲကို reverse ပြန်ထားတဲ့တန်ဖိုးကဝင်သွားတယ်
 		sb.reverse();
 
 //		အဲ့လို ဝင်လာတာကို အောက်ကလို direct စစ်ရင် error အစိမ်းရောင်လေးတက်မယ် error က
-//		StringBuilder က String နဲ့ DataTypes မတူပဲ နှိုင်းယှဉ်ထားလို့ StringBuilder က String ကနေပဲဆင်းသက်လာလို့
+//		StringBuilder က String နဲ့ DataTypes မတူပဲ နှိုင်းယှဉ်ထားလို့ဖြစ်တယ် StringBuilder က String ကနေပဲဆင်းသက်လာလို့
 //		compile time error မတက်ပေမယ့် အမှန်အတိုင်းမလုပ်နိုင်တဲ့ error တက်တယ် အဲ့လိုမဖြစ်အောင် DataTypes တူအောင်ချိန်းရမယ် 
 //		System.out.println(st.equals(sb) ? "Palindrome" : "Not Palindrome");
 		
 //		DataTypes တူအောင်ချိန်းမယ်ဆို StringBuilder ကို String အနေနဲ့ပြောင်းပြီးနှိုင်းယှဉ်ရင် toString() ကိုသုံး ဒီလိုရေးမယ်
 		System.out.println(st.equals(sb.toString()) ? "Palindrome" : "Not Palindrome");
+		
+//		String တစ်ခုကိုကြောညာလိုက်ပြီဆိုရင် memory ပေါ်မှာ 16 bit နေရာသွားယူထားတယ် အဲ့တာကို StringBuilder() ထဲမှာကိုယ်လိုချင်သလောက်ပြင်ရေးနိုင်တယ်
+		StringBuilder sb2 = new StringBuilder(20);
+//		insert သည် sb2 ထဲကို st input ကိုလာထည့်လိုက်ခြင်းဖြစ်သည်
+		sb2.insert(0, st);
+		System.out.println(sb2.length());
+		System.out.println(sb2.capacity());
 		
 		sc.close();
 		
