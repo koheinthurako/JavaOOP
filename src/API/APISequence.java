f7396f605f1ca52132a70cd05dcf3de0eaec5998
koheinthurako <captainthura99@gmail.com>
2023-12-11 23:35:27 +0630

    Test valueOf and parseInt return type

--- src/API/APISequence.java
+++ src/API/APISequence.java
@@ -2,7 +2,8 @@ package API;
 
 public class APISequence {
 	
-//	အစဉ်လိုက်ကြည့်ပါ
+//	API ထဲက ဟာတွေကို အောက်ကအတိုင်း အစဉ်လိုက်ကြည့်ပါ
+	
 //	1. Java.lang.Object
 //	2. Java.lang.String
 //	3. Java.lang.StringBuilder
--- src/API/WrapperClassTest.java
+++ src/API/WrapperClassTest.java
@@ -17,7 +17,7 @@ public class WrapperClassTest {
 //		Wrapper class ဆိုတာက primitive data type 8 ခုကို class အနေနဲ့ ကိုယ်စားပြုပြီး Java မှာ ရေးထားခြင်းဖြစ်တယ်
 //		အဲ့လိုရေးလိုက်ရင် data type တွေက class အဖြစ်ပြောင်းသွားလို့ object တွေဆောက်နိုင်ပြီး သူတို့အထဲက method တွေကိုယူသုံးနိုင်တယ်
 
-//		System.out.println("--- Start wrapper class ---");
+		System.out.println("--- Start wrapper class ---");
 		
 		Integer a = 2;
 		Integer b = 5;
@@ -28,9 +28,33 @@ public class WrapperClassTest {
 //		string() ရဲ့နောက်မှာ string ထပ်ဆက်ရမှာမို့ concat() ကိုသုံးလိုက်တယ် သို့သော် အထဲကတန်ဖိုးတွေက integer အနေနဲ့တွက်ရမှာမို့
 //		a.intValue() ဆိုပြီး integer ပြောင်းလိုက်တယ် သို့သော် concat က string ကိုပဲလက်ခံတာမို့ အထဲက intValue() ပြောင်းပြီးပေါင်းထားတာကို
 //		ကွင်းခတ်ပြီးနောက်မှာ concat ကလက်ခံနိုင်အောင် string ""  အလွတ်တစ်ခုနဲ့ထပ်ပေါင်းပေးလိုက်တယ်
+//		byteValue() longValue() doubleValue() ဆိုပြီးကြိုက်ရာချိန်းနိုင်တယ်
 		System.out.println((a + " + " + b + " = ").toString().concat((a.intValue() + b.intValue()) + ""));
 		
+//		ဒါသည် method အတွေအလွယ်ယူသုံးပြီး အထောက်အကူဖြစ်သည်
+		System.out.println("Integer.MAX_VALUE : " + Integer.MAX_VALUE + "\n");
 		
+//		Boxing & UnBoxing
+//		primitive data type ကနေ Wrapper class ကိုချိန်းရင် boxing
+		Integer it1 = 10;
+		
+//		wrapper class ကနေ primitive data type ကိုချိန်းရင် UnBoxing
+		it1 = it1.intValue();
+		
+//		Type Casting မှာ data type တစ်ခုကို အခြားတစ်ခုကိုပြောင်းရင်နှစ်မျိုးရှိတယ်
+//		parseXxx နဲ့လည်းရသလို xxx.valueOf() နဲ့လည်းရတယ် သို့သော် သူတို့ရဲ့ return ပြန်တန်ဖိုးကကွာတယ်
+		System.out.println("--- Test Type Casting ---");
+		String name = "12";
+		System.out.println(Integer.valueOf(name).toString().charAt(0) + "5");
+//		valueOf() က instance/object ကို return ပြန်တယ် အဲ့ဒါကြောင့် သူ့နောက်မှာ "." ခံပြီး method တွေထပ်ခေါ်လို့ရတယ်
+		int unboxName = Integer.valueOf(name); // UnBoxing
+		System.out.println(unboxName);
+//		ဒါကြောင့် integer တန်ဖိုးထပ်လိုချင်ရင် သူ့ကို UnBoxing ပြန်လုပ်ပေးရတယ်
+		
+		String name2 = "1";
+		System.out.println(Integer.parseInt(name2));
+//		parseInt() က integer တန်ဖိုးကို return ပြန်တယ်
+//		Integer. ဆိုပြီး class ကနေလှမ်းခေါ်ထားပေမယ့် return က integer ဖြစ်သွားလို့ method တွေထပ်ခေါ်မရတော့ပါ
 		
 	}
 
