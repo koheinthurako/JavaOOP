e598aff4738bec99271b45cd07e6de8eea730ec7
koheinthurako <captainthura99@gmail.com>
2023-12-16 23:53:33 +0630

    finish OTP code

--- Assignments/ObjectTestAssignments/OTPCodeChecker.java
+++ Assignments/ObjectTestAssignments/OTPCodeChecker.java
@@ -8,23 +8,80 @@ import java.util.Scanner;
 
 public class OTPCodeChecker {
 
-	public static List<Object> mainList = new ArrayList<Object>();
+	public static List<List<Object>> mainList = new ArrayList<List<Object>>();
+	public static int userOTPCount = 1;
 
 	public static void main(String[] args) throws InterruptedException {
 		
+		System.out.println("Please log in your account with OTP code...");
 		programStart();
-		System.out.println(sendOTPCode());
-		Thread.sleep(5000);
-		OTPDelete();
-		requestUserOTP();
-		
+
 	}
-	
+
+	public static void continueOrNot() throws InterruptedException {
+		System.out.print("Wrong! Do you want to get another OTP code? Y/N : ");
+		Scanner sc = new Scanner(System.in);
+		if (sc.nextLine().toLowerCase().equals("y")) {
+			System.out.println("---------------------------------------------------");
+			if (userOTPCount > 3) {
+				System.out.println("Sorry! There are more than 3 times. Try next day.");
+			} else {
+				programStart();
+			}
+		} else {
+			System.out.println("Program Ended! Enjoy your day!");
+		}
+		sc.close();
+	}
+
+	public static void checkOTP(String inputOTP) throws InterruptedException {
+		for (List<Object> otpList : mainList) {
+			StringBuilder otpToString = new StringBuilder();
+			for (Object otp : otpList) {
+				otpToString.append(otp);
+			}
+			String result = otpToString.toString();
+			if (inputOTP.equals(result)) {
+				System.out.println("Correct! You haved logged in to your account");
+				return;
+			}
+		}
+		continueOrNot();
+	}
+
+	public static void requestUserOTP() throws InterruptedException {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter your OTP Code : ");
+		String inputOTP = sc.nextLine();
+		checkOTP(inputOTP);
+		sc.close();
+	}
+
+	public static void OTPDelete() {
+		for (int i = 0; i < 30; i++) {
+			System.out.println("");
+		}
+	}
+
+	public static String sendOTPCode() {
+		System.out.print("Here is your OTP Code : ");
+		List<Object> shuffledList = new ArrayList<>(mainList);
+		Collections.shuffle(shuffledList);
+		@SuppressWarnings("unchecked")
+		// shuffledList.get() က object return ပြန်တာကို List<Object> အနေနဲ့ type casting ပြန်လုပ်
+		List<Object> result = (List<Object>) shuffledList.get(0);
+		StringBuilder otpStringBuilder = new StringBuilder();
+		for (Object otp : result) {
+			otpStringBuilder.append(otp);
+		}
+		return otpStringBuilder.toString();
+	}
+
 	public static List<Object> generateOTPCode() {
-		List<Object> dataAcceptList = new ArrayList<Object>(); 
+		List<Object> dataAcceptList = new ArrayList<Object>();
 		Random rCode = new Random();
-		for(int i = 0; i < 6; i++) {
-			if(i > 3) {
+		for (int i = 0; i < 6; i++) {
+			if (i > 3) {
 				dataAcceptList.add(rCode.nextInt(10));
 			} else {
 				dataAcceptList.add((char) (rCode.nextInt(26) + 'a'));
@@ -32,52 +89,16 @@ public class OTPCodeChecker {
 		}
 		return dataAcceptList;
 	}
-	
-	public static void programStart() {
-		for(int i = 0; i < 10; i++) {
+
+	public static void programStart() throws InterruptedException {
+		for (int i = 0; i < 10; i++) {
 			mainList.add(generateOTPCode());
 		}
+		System.out.println(sendOTPCode());
+		userOTPCount++;
+		Thread.sleep(5000);
+		OTPDelete();
+		requestUserOTP();
 	}
-	
-	public static String sendOTPCode() {
-		System.out.print("Here is your OTP Code : ");
-		List<Object> shuffledList = new ArrayList<>(mainList);
-        Collections.shuffle(shuffledList);
-        @SuppressWarnings("unchecked")
-        // shuffledList.get() က object return ပြန်တာကို List<Object> အနေနဲ့ type casting ပြန်လုပ်
-		List<Object> result = (List<Object>) shuffledList.get(0);
-        StringBuilder otpStringBuilder = new StringBuilder();
-        for(Object otp : result) {
-        	otpStringBuilder.append(otp);
-        }
-        return otpStringBuilder.toString();
-	}
-	
-	public static void OTPDelete() {
-		for(int i = 0; i < 30; i++) {
-			System.out.println("");
-		}
-	}
-	
-	public static void confirmation() {
-		System.out.println("True");
-	}
-	
-	public static void requestUserOTP() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Please enter your OTP Code : ");
-		try {
-			while(!mainList.isEmpty()) {
-				for(Object checkOTP : mainList) {
-					if(sc.nextLine().equals(checkOTP)) {
-						confirmation();
-						break;
-					}
-				}
-			}
-		} catch (Exception e) {
-			
-		}
-	}
-	
+
 }
