2105152aa1190d22b0e414dab40fc4ba7c735224
koheinthurako <captainthura99@gmail.com>
2023-12-24 23:05:21 +0630

    more about handling

--- src/FileHandling/FileHandlingTest.java
+++ src/FileHandling/FileHandlingTest.java
@@ -15,7 +15,7 @@ public class FileHandlingTest {
 	public static void main(String[] args) {
 
 //		File Handling အကြောင်းစမယ်
-		
+
 //		FileSystem fs = FileSystems.getDefault();
 //		for(FileStore store : fs.getFileStores()) {
 //			System.out.println("store.toString() : " + store.toString());
@@ -24,7 +24,7 @@ public class FileHandlingTest {
 //			System.out.println("UsableSpace : " + store.getUsableSpace());
 //			System.out.println("UnallocatedSpace : " + store.getUnallocatedSpace());
 //		}
-		
+
 //		1. Path - absolute path, relative path
 //		absolute path က root directory ကနေစယူပြီး relative path က current path ကနေစယူ
 		try {
@@ -34,12 +34,15 @@ public class FileHandlingTest {
 //			path.toFile() = path မှ file ကိုပြောင်းပြီး getDesktop() နဲ့လှမ်းဖွင့်
 //			တစ်ခု create လုပ်ပြီးသားရှိပြီးသားကို ထပ်လုပ်ရင် FileAlreadyExistsException error တက်
 			Desktop.getDesktop().open(path.toFile());
+//			IO Exception မရေးခင်မှာမှ catch ထဲမှာ fileAlready...exception ကို အရင်ဖမ်းပေးရမယ် မရေးရင် error ပြ
+//			ဘာလို့ဆို IO စစ်ပြီးမှ file ကိုသွားစစ်လို့မရဘူး file ထပ်မထပ်စစ်ပြီးမှ IO အဆင်ပြေမပြေစစ်ပေးရတာမို့လို့
+		} catch (FileAlreadyExistsException e2) {
+			System.err.println("File already exists!");
+//			IOException ဆိုတာ create ပေးလိုက်တဲ့/ အလုပ်လုပ်လိုက်တဲ့ path လမ်းကြောင်းတစ်ခုက input/output တွေအဆင်ပြေမပြေကိုပဲဖမ်းပေးတယ်
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
 
-		
 	}
 
 }
