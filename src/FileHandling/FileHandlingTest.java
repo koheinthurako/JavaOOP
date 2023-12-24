6e4cc32b7bd0fc73dc42081a02ede8e6bdd12fbb
koheinthurako <captainthura99@gmail.com>
2023-12-24 22:58:26 +0630

    start about path

--- src/FileHandling/FileHandlingTest.java
+++ src/FileHandling/FileHandlingTest.java
@@ -1,13 +1,18 @@
 package FileHandling;
 
+import java.awt.Desktop;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileStore;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileHandlingTest {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 //		File Handling အကြောင်းစမယ်
 		
@@ -20,8 +25,20 @@ public class FileHandlingTest {
 //			System.out.println("UnallocatedSpace : " + store.getUnallocatedSpace());
 //		}
 		
-//		1. Path
+//		1. Path - absolute path, relative path
+//		absolute path က root directory ကနေစယူပြီး relative path က current path ကနေစယူ
+		try {
+			Path path = Paths.get("D:\\fileHandlingTest.txt");
+//			createFile - file တစ်ခုဖန်တီးခြင်း
+			Files.createFile(path);
+//			path.toFile() = path မှ file ကိုပြောင်းပြီး getDesktop() နဲ့လှမ်းဖွင့်
+//			တစ်ခု create လုပ်ပြီးသားရှိပြီးသားကို ထပ်လုပ်ရင် FileAlreadyExistsException error တက်
+			Desktop.getDesktop().open(path.toFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+
 		
 	}
 
