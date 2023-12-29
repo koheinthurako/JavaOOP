0aa98fd99dfcf3965fc1a66d81d8eef4ab30064d
koheinthurako <captainthura99@gmail.com>
2023-12-29 20:13:00 +0630

    diary concept finished

--- Assignments/FileHandling/Diary.java
+++ Assignments/FileHandling/Diary.java
@@ -2,10 +2,14 @@ package FileHandling;
 
 import java.awt.Desktop;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Diary {
@@ -32,12 +36,12 @@ public class Diary {
 	}
 
 	public static void checkFile() {
-		String name = fileName;
-		Path path = Paths.get("D://" + folderName + "/" + name + fileType);
+//		D://Diary/diary.txt
+		Path path = Paths.get("D://" + folderName + "/" + fileName + fileType);
 		try {
 			Files.createFile(path);
-			addTexts();
 		} catch (FileAlreadyExistsException e) {
+			addTexts(path);
 			try {
 				Desktop.getDesktop().open(path.toFile());
 			} catch (IOException e1) {
@@ -46,14 +50,32 @@ public class Diary {
 		} catch (IOException e2) {
 			e2.printStackTrace();
 		}
-		System.out.println(name);
 	}
 
-	public static void addTexts() {
+	public static void addTexts(Path path) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Write notes...");
-		
-		
+		System.out.println("Program Start! Write notes...");
+		LocalDate date = LocalDate.now();
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy (E)");
+		String formattedDate = date.format(dateFormatter);
+		try {
+			int no = 1;
+			Files.writeString(path, formattedDate + System.lineSeparator(), Charset.forName("UTF-8"), StandardOpenOption.CREATE);
+			while(true) {
+				String input = sc.nextLine();
+				Files.writeString(path, no + ". " + input + System.lineSeparator(), Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+				++no;
+//				System.out.println("Wanna stop writting? Y/N : ");
+//				if(sc.nextLine().toLowerCase().equals("y")) {
+//					System.out.println("Program Ended!");
+//					break;
+//				};
+			}
+			
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+		sc.close();
 	}
 	
 }
